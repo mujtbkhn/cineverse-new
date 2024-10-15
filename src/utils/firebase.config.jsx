2d427fa7d3696dd01afc520794e7e9547b542608
abdul rehman mujtaba <mujjukhn538@gmail.com>
2024-10-15 13:34:37 +0530

    uploading

--- src/utils/firebase.config.jsx
+++ src/utils/firebase.config.jsx
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: "netflixgpt-3cc18",
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
